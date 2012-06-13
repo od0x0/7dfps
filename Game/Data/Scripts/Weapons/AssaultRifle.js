fac1a0bb9eef8485a6ca2a25df2736c7233c937a
Bink <erik_kemmer@yahoo.de>
2012-06-13 14:01:48 +0200

    Changed the Assault Rifle settings a bit to place it correctly. Added some animation and effects.

--- Game/Data/Scripts/Weapons/AssaultRifle.js
+++ Game/Data/Scripts/Weapons/AssaultRifle.js
@@ -15,9 +15,9 @@ function OnConstruct(weap,subEvent,id,tick)
 
 		// setup how weapon is held in hand
 		
-	weap.handPosition.x=-250;
+	weap.handPosition.x=-350;
 	weap.handPosition.y=-900;
-	weap.handPosition.z=1800;
+	weap.handPosition.z=1600;
 	
 	weap.handAngle.x=0;
 	weap.handAngle.y=0;
