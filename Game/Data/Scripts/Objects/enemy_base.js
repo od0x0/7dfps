bfb3b2f912374c1f0b93c83958a3d724f63256b8
Aaron Srdoc <trianglebutt@gmail.com>
2012-06-14 15:09:49 -0400

    Map 2 little more done.

--- Game/Data/Scripts/Objects/enemy_base.js
+++ Game/Data/Scripts/Objects/enemy_base.js
@@ -337,6 +337,7 @@ function enemyDie(obj,subEvent,id,tick) {
     obj.watch.stop();
     obj.setting.contact = false;
     obj.setting.damage = false;
+	sound.play('Robot Explosion 1',obj.position,1.0);
 }
 
 // being targetted
